70bddcd34acbbba6082c5b832be0904bd646c903
karamaru <mrnk3078@gmail.com>
2022-07-09 23:20:05 +0900

    a

--- go/main.go
+++ go/main.go
@@ -1592,7 +1592,7 @@ func postShip(w http.ResponseWriter, r *http.Request) {
 	tx := dbx.MustBegin()
 
 	item := Item{}
-	err = tx.Get(&item, "SELECT  * FROM `items` WHERE `id` = ? FOR UPDATE", itemID)
+	err = tx.Get(&item, "SELECT * FROM `items` WHERE `id` = ? FOR UPDATE", itemID)
 	if err == sql.ErrNoRows {
 		outputErrorMsg(w, http.StatusNotFound, "item not found")
 		tx.Rollback()
